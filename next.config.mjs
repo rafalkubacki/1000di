b8e331a6d5e88a32a162fc7451d703f0d374561b
Rafał Kubacki <rafalkubacki7@gmail.com>
2024-08-06 20:00:55 +0200

    chore: Update image domain for lazy loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['eu-central-1-shared-euc1-02.graphassets.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'eu-central-1-shared-euc1-02.graphassets.com',
+        port: '',
+      },
+    ],
   },
 };
 
